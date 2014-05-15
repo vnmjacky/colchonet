204e13afea8cf6d61c11b6b4af883ebced75a807
jack <vnmjacky@gmail.com>
2014-05-14 22:14:52 -0300

    ide files i18n mail start

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :rooms, :users
+
+  scope "(:locale)", locale: /en|pt/ do
+    resources :rooms, :users
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get '/:locale' => 'home#index', locale: /en|pt/
   # You can have the root of your site routed with "root"
   root 'home#index'
 
