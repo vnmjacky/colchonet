90e2540e8ea1f2a6798301096614fc573485c190
jack <william.weckl@gmail.com>
2014-05-21 21:39:21 -0300

    user confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   
   #Validations
-  validates_presence_of :email, :full_name, :location, :password
+  validates_presence_of :email, :full_name, :location
   #validates_confirmation_of :password
   validates_length_of :bio, minimum: 30, allow_blank: false
   validates_format_of :email, with: EMAIL_REGEXP
