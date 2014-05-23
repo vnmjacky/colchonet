b661a3cc694f07641cf515c87703c0a6434399ce
jack <william.weckl@gmail.com>
2014-05-22 22:10:57 -0300

    logged in and logged out validations

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,6 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update, :destroy]
+  before_action :require_authentication, only: [:new, :edit, :create, :update, :destroy]
 
   # GET /rooms
   # GET /rooms.json
@@ -28,7 +29,7 @@ class RoomsController < ApplicationController
 
     respond_to do |format|
       if @room.save
-        format.html { redirect_to @room, notice: 'Room was successfully created.' }
+        format.html { redirect_to @room, notice: t('flash.notice.room_created') }
         format.json { render :show, status: :created, location: @room }
       else
         format.html { render :new }
@@ -42,7 +43,7 @@ class RoomsController < ApplicationController
   def update
     respond_to do |format|
       if @room.update(room_params)
-        format.html { redirect_to @room, notice: 'Room was successfully updated.' }
+        format.html { redirect_to @room, notice: t('flash.notice.room_updated') }
         format.json { render :show, status: :ok, location: @room }
       else
         format.html { render :edit }
@@ -56,7 +57,7 @@ class RoomsController < ApplicationController
   def destroy
     @room.destroy
     respond_to do |format|
-      format.html { redirect_to rooms_url, notice: 'Room was successfully destroyed.' }
+      format.html { redirect_to rooms_url, notice: t('flash.notice.room_destroyed') }
       format.json { head :no_content }
     end
   end
